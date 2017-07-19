3620278028b00b4841b1e92661213fdcca595f6c
rmit-s3459784-Campbell-Brobbel <s3459784@student.rmit.edu.au>
2017-07-19 18:35:52 +1000

    First Commit

---
+++ iOSAssignment1/iOSAssignment1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSAssignment1
+//
+//  Created by Campbell Brobbel on 19/7/17.
+//  Copyright © 2017 CampbellRhys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
