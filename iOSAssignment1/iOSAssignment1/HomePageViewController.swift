51e78d2f361a501d109676a6a638f71d0abef31c
rmit-s3459784-Campbell-Brobbel <s3459784@student.rmit.edu.au>
2017-08-08 13:24:53 +1000

    Storyboards created

---
+++ iOSAssignment1/iOSAssignment1/HomePageViewController.swift
@@ -0,0 +1,50 @@
+//
+//  HomePageViewController.swift
+//  iOSAssignment1
+//
+//  Created by Campbell Brobbel on 8/8/17.
+//  Copyright © 2017 CampbellRhys. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIPageViewController, UIPageViewControllerDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - Page View Controller Data Source
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        return nil
+    }
+    
+    // MARK: - Page View Controller Delegate
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
